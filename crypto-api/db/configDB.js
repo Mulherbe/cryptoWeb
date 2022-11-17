15dfc0cc46a67cad7d7a00531adcb341007bbed7
laBHaZe06 <borne.yoan@outlook.fr>
2022-11-17 11:32:17 +0100

    setup connection to mysql

--- crypto-api/db/configDB.js
+++ crypto-api/db/configDB.js
@@ -1,2 +1,25 @@
+var mysql = require('mysql');
+
+const dbuser = process.env.DB_USER;
+const dbpwd = process.env.DB_PWD;
+const dbName = process.env.DB_NAME;
+const dbHost = process.env.DB_HOST;
+
+var connection = mysql.createConnection({
+    host: dbHost,
+    user: db,
+    password: dbpwd,
+    database: dbName,
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Result: " + result);
+      });
+});
+
 
 
