c06eb25787be801359ad33470d4f7f637274d9ff
laBHaZe06 <borne.yoan@outlook.fr>
2022-11-19 00:53:56 +0100

    update docker compose with sequelize and mysql connection + user model ok

--- crypto-api/app.js
+++ crypto-api/app.js
@@ -1,23 +1,30 @@
+require('rootpath')();
+//Petite aide pour rendre node.js require relatif à la racine de votre projet
+//ROOTH_PATH = __dirname;
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 5000;
-const dataBase = require('./db/configDB');
+require('dotenv').config();
+
+//const database = require('db/configDB');
 
 app.use(express.json());
-//const cors = require('cors');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
+// Make public static folder
+app.use(express.static("public"));
+const errorHandler = require('middleware/error-handler');
 
 app.get('/', (req,res) => {
     res.send('Home Route')
 });
+app.use('/api/users', require('model/User/User.controller'));
+app.use(errorHandler);
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-
-// Make public static folder
-app.use(express.static("public"));
-
+const port = process.env.PORT || 5001;
 app.listen(port, () => {
     console.log(`🚀 Server is running on port : ${port} 🚀`);
 });
---
+++ crypto-api/helper/db.js
@@ -0,0 +1,40 @@
+const mysql = require('mysql2/promise');
+const { Sequelize } = require('sequelize');
+const dbhost = process.env.DB_HOST;
+const dbport = process.env.DB_PORT;
+const dbuser = process.env.DB_USER;
+const dbpwd = process.env.DB_PWD;
+const dbName = process.env.DB_NAME;
+
+module.exports = db = {};
+
+initialize();
+async function initialize() {
+    // création de la db si elle n'existe pas
+    //console.log('start')
+    const connection = mysql.createPool({
+        host: dbhost,
+        port: dbport,
+        user: dbuser,
+        password: dbpwd,
+        multipleStatements: true
+    });
+    const [rows, fields] = await connection.query(`CREATE DATABASE IF NOT EXISTS ${dbName};`);
+    //console.log('end')
+    // connexion à la db
+    const sequelize = new Sequelize(
+        dbName,
+        dbuser,
+        dbpwd,
+        {
+            host: dbhost,
+            dialect: "mysql",
+        }
+    );
+
+    // initialisation des modèles   
+    db.User = require('model/User/User.model')(sequelize);
+
+    // sync tout les models de la db
+    await sequelize.sync({ alter: true });
+}
