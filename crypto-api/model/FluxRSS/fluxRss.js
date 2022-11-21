a2ea7ba90fd29c91817c16c21e1ee350ce9042d4
laBHaZe06 <borne.yoan@outlook.fr>
2022-11-21 11:08:35 +0100

    update

--- crypto-api/model/FluxRSS/fluxRss.js
+++ crypto-api/model/FluxRSS/fluxRss.js
@@ -3,13 +3,11 @@ const Feed = require("feed");
 
 // Initialisation du module
 const feed = new Feed({
-    
     title: "Titre de mon fil de syndication",
     description: "Description de mon fil!",
     link: "http://example.com/",
     image: "http://example.com/logo.png",
     copyright: "X11 License",
-
     author: {
         name: "Monsieur X",
         email: "monsieur.x@example.com",
