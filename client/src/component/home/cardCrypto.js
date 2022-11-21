a540e9b53166b15c8cb11bfcf2a62a913f1835e4
Mulherbe <fredericmuller06@hotmail.com>
2022-11-21 17:23:26 +0100

    add component for home,crypto and news

---
+++ client/src/component/home/cardCrypto.js
@@ -0,0 +1,16 @@
+import React , { useEffect } from 'react';
+import '../../assets/css/style.css';
+
+
+const CardCrypto = () => {
+
+  return (
+    <>
+        <div className='container_CardCrypto'>
+        
+      </div>
+    </>
+  ); }
+
+
+export default CardCrypto;
---
+++ client/src/component/home/news.js
@@ -0,0 +1,17 @@
+import React , { useEffect } from 'react';
+import '../../assets/css/style.css';
+
+
+const News = () => {
+
+
+  return (
+    <>
+        <div >
+        News
+      </div>
+    </>
+  ); }
+
+
+export default News;
