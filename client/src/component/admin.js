e474de9c44f5fbe28e921e68b2068e41c6199afa
Mulherbe <fredericmuller06@hotmail.com>
2022-11-21 16:29:14 +0100

    add page profil, need just to link with api

---
+++ client/src/component/admin.js
@@ -0,0 +1,21 @@
+import React , { useEffect } from 'react';
+import './../assets/css/style.css';
+import bg from "../assets/img/bg.jpg"; 
+
+
+
+const Admin = () => {
+
+
+  return (
+    <>
+        <div class="container">
+      
+
+      </div>
+  
+    </>
+  ); }
+
+
+export default Admin;
