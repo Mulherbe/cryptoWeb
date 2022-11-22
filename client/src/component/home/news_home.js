f8733f6486ec647b729a2f0e685bf1c6824776d8
Mulherbe <fredericmuller06@hotmail.com>
2022-11-22 14:36:05 +0100

    add date flux rss

--- client/src/component/home/news_home.js
+++ client/src/component/home/news_home.js
@@ -6,22 +6,27 @@ import '../../assets/css/style.css';
 const News_home = () => {
 
   const [actuTab, setActuTab] = useState();
-  
-const  createActu  = () => {
-    getActu().then(response=>setActuTab(
-      Object.keys(response.data).map((key, index) => {
-      return (
-        <div key={index}>
-          {console.log(response.data[key].title)}
-         <a href={response.data[key].link} target="_blank">{response.data[key].title} 
+  const [dateFr, setDateFr] = useState();
 
-          </a> 
-          <hr />
-        </div>
-      );
-    }))
-  )
-    
+  function createActu() {
+    getActu().then(response => setActuTab(
+      Object.keys(response.data).map((key, index) => {
+        console.log(response.data[key].date.toString())
+        return (
+          <div key={index}>
+            <a href={response.data[key].link} target="_blank">{response.data[key].title}
+            </a>
+            {new Intl.DateTimeFormat("en-GB", {
+            year: "numeric",
+            month: "long",
+            day: "2-digit"
+          }).format(response.data[key].date)}
+            <hr />
+          </div>
+        );
+      }))
+    );
+
   }
 
   
