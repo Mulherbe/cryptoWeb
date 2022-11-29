bf0edfe7475fc68a020be759dc355de9c7da99b1
Mulherbe <fredericmuller06@hotmail.com>
2022-11-29 11:13:42 +0100

    update responsive nav bar

--- client/src/component/home/cardCrypto.js
+++ client/src/component/home/cardCrypto.js
@@ -2,20 +2,9 @@ import React , { useEffect, useState } from 'react';
 import '../../assets/css/style.css';
 import { MiniChart } from "react-ts-tradingview-widgets";
 
-
 const CardCrypto = (props) => {
         const [pass, setPass] = useState('1');
 
-const cryptoDetails = () => {
-  var iframevar = document.getElementById('minichart');
-  var child = iframevar.getElementsByTagName( 'div' )[0];
-  var child2 = child.getElementsByTagName( 'div' )[0];
-  var child3 = child2.getElementsByTagName( 'div' )[0];
-  var child4 = child3.getElementsByTagName( 'iframe' )[0];
-   var elmnt = child4.contentWindow.document.getElementById("mini-symbol-overview");
-
-  console.log(elmnt)
-}
 
 let cryptoSymbol = 'BINANCE:' + props.crypto.replace('/', '')
 
@@ -23,15 +12,14 @@ let cryptoSymbol = 'BINANCE:' + props.crypto.replace('/', '')
     <>
 
         <div className='container_CardCrypto'>
-          <div class="click_test"  onClick={cryptoDetails} >
-          </div>
+          {/* <div class="click_test"  onClick={cryptoDetails} >
+          </div> */}
           <div id="minichart">
             <MiniChart symbol={cryptoSymbol}
              colorTheme="light"  isTransparent="true" width="100%"></MiniChart>
+             
           </div>
-          <div   onClick={cryptoDetails} >
-            test
-          </div>
+
       </div>
     </>
   ); }
