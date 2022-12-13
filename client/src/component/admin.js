bb6d238423fd4f0f5274ce76ae0f0b884f8d7790
Marco <marco.gazzera@epitech.eu>
2022-12-13 19:04:12 +0100

    Clear

--- client/src/component/admin.js
+++ client/src/component/admin.js
@@ -1,10 +1,8 @@
-import React , { useEffect} from 'react';
 import './../assets/css/style.css';
 import CardFlux from './home/CardFlux';
 import CardCrpto2 from './home/CardListCrypto';
 import CardUsers from './home/CardUser';
 import SearchBar from './home/searchBar';
-// import logo from '../../assets/img/logo.png';
 
 const Admin = () => {
   return (
