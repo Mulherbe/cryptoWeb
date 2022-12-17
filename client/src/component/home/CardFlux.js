c0954db2a70ef2f7121b85bd751669b17ea070f5
Marco <marco.gazzera@epitech.eu>
2022-12-17 14:27:12 +0100

    Print Card v0

--- client/src/component/home/CardFlux.js
+++ client/src/component/home/CardFlux.js
@@ -1,5 +1,7 @@
-// import React , { useEffect } from 'react';
+import React , { useState } from 'react';
+import { render } from 'react-dom';
 import '../../assets/css/style.css';
+
 
 import News_home from './news_home';
 import NewsNFT from './news_nft';
@@ -7,33 +9,40 @@ import NewsBTC from './news_BTC';
 import NewsETH from './news_ETH';
 
 function CardFlux(props) {
- 
+  render() 
+    const [showCard, setShowCard] = useState(false); 
+    
   return (
     <div className='container_CardAdmin'>
       <h2> <u>{props.name}</u></h2>
       <div className='container_button'>
-        <button type="button" className="nav_button" >{props.actu}</button>
+        <button type="button" className="nav_button" onClick={() => setShowCard(true)} >{props.actu}</button>
+        {showCard && <News_home />}
         <button type="button" className="nav_button">{props.btc}</button>
         <button type="button" className="nav_button">{props.eth}</button>
         <button type="button" className="nav_button">{props.nft}</button>
       </div>
-      <div className='container_tabFlux'>          
-      <table>
+      {/* <div className='container_tabFlux'>          
+       {/* <table>
         <tr>
           <th> 
             <div class="container_news" id="style-1">
-              <td><NewsETH/></td>
-              {/* <NewsNFT/> */}
+
+              <td><News_home/></td>
+              {/* 
+                <NewsNFT/> 
+                <NewsBTC/>
+                <NewsETH/>              
+              
 
 
             </div>
           </th>
         </tr>
-      </table>
-      </div>
+      </table> 
+      </div> */}
     </div>  
-    )
-
-}
+    );
+  }
 
 export default CardFlux;
